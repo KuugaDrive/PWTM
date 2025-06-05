32f28921b02291e270e14e0c2f6370b2d04f230c
RealXEyez <132649141+KuugaDrive@users.noreply.github.com>
2025-06-05 10:34:59 +0700

    Shu

---
+++ UAS_Belajar/time.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time</title>
+</head>
+<body>
+    <!-- <h2>My First JavaScript</h2>
+
+    <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">Klik untuk menampilkan waktu</button>
+
+    <p id="demo"></p>  -->
+
+
+    <button onclick="document.getElementById('myImage').src='img/pic_bulbon.gif'">Nyalakan Lampu</button>
+
+    <img id="myImage" src="img/pic_bulboff.gif" style="width: 100px;" alt="">
+
+    <button onclick="documnet.getElementById('myImage').src='img/pic_bulboff.gif'">Matikan Lampu</button>
+</body>
+</html>
